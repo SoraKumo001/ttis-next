3b1beec78453b65705af802781a73ef71380b317
SoraKumo <info@croud.jp>
2020-05-08 20:34:33 +0900

    fix logout

--- ttis-front/src/pages/api/logout.ts
+++ ttis-front/src/pages/api/logout.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 export default (req: Request, res: Response) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  if (req.body.graphqlToken === req.session.graphqlToken) {
-    delete req.session.graphqlToken;
+  if (req.body.graphqlToken === req.session?.graphqlToken) {
+    delete req.body.graphqlToken;
   }
   res.end('{result:"OK"}');
 };
